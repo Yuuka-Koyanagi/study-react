57380579d78259b6d499c47147c3cfb70140be69
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-03-24 19:09:48 +0900

    ♻️ Linksのリファクタリング

--- components/WelcomePage/Links.js
+++ components/WelcomePage/Links.js
@@ -1,32 +1,44 @@
 import styles from "./Links.module.css";
 import Image from "next/image";
 
+const ITEMS = [
+  {
+    className: styles.primary,
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    image:
+      <Image
+        className={styles.logo}
+        src="/vercel.svg"
+        alt="Vercel logomark"
+        width={20}
+        height={20}
+      />,
+    discription: "Deploy now"
+  },
+  {
+    className: styles.secondary,
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    discription: "Read our docs"
+  }
+];
+
 export const Links = () => {
   return (
     <div className={styles.ctas}>
-      <a
-        className={styles.primary}
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.logo}
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={20}
-          height={20}
-        />
-        Deploy now
-      </a>
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.secondary}
-      >
-        Read our docs
-      </a>
+      {ITEMS.map(item => {
+        return (
+          <a
+            key={item.href}
+            className={item.className}
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.image}
+            {item.discription}
+          </a>
+        );
+      })}
     </div>
   );
 } 
