9b0ea58aa5e2f1e21cbedac71f601ca1f92ddcda
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-04-07 15:57:01 +0900

    ✨ 講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+
 import { WelcomePage } from "@/components/WelcomePage";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightBlue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("アンマウント時");
+    }
+  }, []);
+
   return <WelcomePage fileName="index" />;
 };
