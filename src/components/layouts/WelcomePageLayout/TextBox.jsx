92523ee3287334c1967e6171fa3ff36c38186220
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-04-16 13:02:02 +0900

    ✨ useStateで配列を扱ってみた

--- src/components/layouts/WelcomePageLayout/TextBox.jsx
+++ src/components/layouts/WelcomePageLayout/TextBox.jsx
@@ -8,5 +8,28 @@ export const TextBox = () => {
       ? setText(e.target.value.trim())
       : alert("5文字以内にしてください");
 
-  return <input type="text" value={text} onChange={handleChange} />;
+  const [array, setArray] = useState([]);
+
+  const handleAdd = () => {
+    setArray(a => {
+      if (a.includes(text)) {
+        alert("同じ値が含まれています");
+        return a;
+      };
+
+      return [...a, text];
+    });
+  };
+
+  return (
+    <>
+      <input type="text" value={text} onChange={handleChange} />
+
+      <button onClick={handleAdd}>追加</button>
+
+      <ul>
+        {array.map((v, i) => <li key={i}>{v}</li>)}
+      </ul>
+    </>
+  );
 };
